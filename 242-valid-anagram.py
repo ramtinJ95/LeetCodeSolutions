b2feaca45ad24b65bc10af6299a6b6010ea0782e
ramtinJ95 <ramme95@gmail.com>
2022-05-23 21:33:05 +0200

    new leetcode for today

--- 242-valid-anagram.py
+++ 242-valid-anagram.py
@@ -11,7 +11,6 @@ def anagramCheck(s, t):
     for i in range(s_len):
         s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
         t_dict[t[i]] = 1 + t_dict.get(t[i], 0)
-        
     for e in s_dict:
         if s_dict[e] != t_dict.get(e, 0):
             return False
