ca4344769fa38f9206f5f08d87eb47ec84a9d30c
Ramtin Javanmardi <ramme95@gmail.com>
2023-01-22 23:49:54 +0100

    vim practice by cleaning up whitespaces etc

--- pythonCode/121-best-time-to-buy-sell-stock.py
+++ pythonCode/121-best-time-to-buy-sell-stock.py
@@ -1,7 +1,9 @@
 from typing import List
 
-prices= [7,1,5,3,6,4]
-# Very similar to problem 53. A variant of kadens algorithm again. 
+prices = [7, 1, 5, 3, 6, 4]
+# Very similar to problem 53. A variant of kadens algorithm again.
+
+
 def maxProfit(prices: List[int]) -> int:
     max_profit = 0
     min_price = 99999
@@ -11,5 +13,6 @@ def maxProfit(prices: List[int]) -> int:
         if price - min_price > max_profit:
             max_profit = price - min_price
     return max_profit
+
 
 print(str(maxProfit(prices)))
