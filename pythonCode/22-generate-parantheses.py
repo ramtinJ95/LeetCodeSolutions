5c6aaeca63cd9a71e4456cbdaf8bbbfd350cb920
ramtinJ95 <ramme95@gmail.com>
2023-04-09 21:33:47 +0200

    another puzzle solved, this one was a bit trickier

---
+++ pythonCode/22-generate-parantheses.py
@@ -0,0 +1,16 @@
+def generateParanthesis(n: int):
+    result = []
+    left = right = 0
+    q = [(left, right, "")]
+    while q:
+        left, right, s = q.pop()
+        if len(s) == 2 * n:
+            result.append(s)
+        if left < n:
+            q.append((left + 1, right, s + "("))
+        if right < left:
+            q.append((left, right + 1, s + ")"))
+    return result
+
+
+print(generateParanthesis(3))
